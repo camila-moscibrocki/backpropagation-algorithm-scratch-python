5c2ddec4a8a2892813acfdfa7eaf6c3882540376
camila-moscibrocki <camilamoscibrocki@gmail.com>
2020-03-03 10:24:22 -0300

    inicio dos testes de aprendizagem

--- seeds.py
+++ seeds.py
@@ -16,10 +16,12 @@ def load_csv(filename):
             dataset.append(row)
         return dataset
 
+
 # Converte a string 'column' em float
 def str_column_to_float(dataset, column):
     for row in dataset:
         row[column] = float(row[column].strip())
+
 
 # Converte a string 'column' em um integrador
 def str_column_to_int(dataset, column):
@@ -32,17 +34,20 @@ def str_column_to_int(dataset, column):
         row[column] = lookup[row[column]]
     return lookup
 
+
 # Encontra os valores maximos e minimos de cada coluna
-def dataset_minmax(datase):
+def dataset_minmax(dataset):
     minmax = list()
-    stats =[[min(column), max(column)] for column in zip(*dataset)]
+    stats = [[min(column), max(column)] for column in zip(*dataset)]
     return stats
+
 
 # reestrutura as colunas do conjunto de dados em uma escala de 0 - 1
 def normalize_dataset(dataset, minmax):
     for row in dataset:
-        for i in range(len(row)-1):
-            row[i] = (row[i] - minmax[i][0])/ (minmax[i][1] - minmax[i][0])
+        for i in range(len(row) - 1):
+            row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
+
 
 # divide o conjunto de dados em subconjuntos k
 def cross_validation_split(dataset, n_folds):
@@ -57,10 +62,159 @@ def cross_validation_split(dataset, n_folds):
         dataset_split.append(fold)
     return dataset_split
 
+
 # calcula a porcentagem de acerto
-def acuracy_metric(actual, predicted):
+def accuracy_metric(actual, predicted):
     correct = 0
-    for i in  range(len(actual)):
+    for i in range(len(actual)):
         if actual[i] == predicted[i]:
             correct += 1
     return correct / float(len(actual)) * 100.0
+
+
+# avalia o algoritimo utilizando validação cruzada
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+    folds = cross_validation_split(dataset, n_folds)
+    scores = list()
+    for fold in folds:
+        train_set = list(folds)
+        train_set.remove(fold)
+        train_set = sum(train_set, [])
+        test_set = list()
+        for row in fold:
+            row_copy = list(row)
+            test_set.append(row_copy)
+            row_copy[-1] = None
+        predicted = algorithm(train_set, test_set, *args)
+        actual = [row[-1] for row in fold]
+        accuracy = accuracy_metric(actual, predicted)
+        scores.append(accuracy)
+    return scores
+
+
+# Calculo do neuronio de ativação para entrada
+def activate(weights, inputs):
+    activation = weights[-1]
+    for i in range(len(weights) - 1):
+        activation += weights[i] * inputs[i]
+    return activation
+
+# Transferência ativação do neurônio
+def transfer(activation):
+    return 1.0 / (1.0 + exp(-activation))
+
+
+# Entrada (propagação futura) para saída (rede)
+def forward_propagate(network, row):
+    inputs = row
+    for layer in network:
+        new_inputs = []
+        for neuron in layer:
+            activation = activate(neuron['weights'], inputs)
+            neuron['output'] = transfer(activation)
+            new_inputs.append(neuron['output'])
+        inputs = new_inputs
+    return inputs
+
+
+# Calcula a derivada de uma saída de neurônio
+def transfer_derivative(output):
+    return output * (1.0 - output)
+
+
+# Erro de retropropagação e armazenamento em neuronios
+def backward_propagate_error(network, expected):
+    for i in reversed(range(len(network))):
+        layer = network[i]
+        errors = list()
+        if i != len(network) - 1:
+            for j in range(len(layer)):
+                error = 0.0
+                for neuron in network[i + 1]:
+                    error += (neuron['weights'][j] * neuron['delta'])
+                errors.append(error)
+        else:
+            for j in range(len(layer)):
+                neuron = layer[j]
+                errors.append(expected[j] - neuron['output'])
+        for j in range(len(layer)):
+            neuron = layer[j]
+            neuron['delta'] = errors[j] * transfer_derivative(neuron['output'])
+
+
+# Atualizando os pesos da rede de acordo com o erro
+def update_weights(network, row, l_rate):
+    for i in range(len(network)):
+        inputs = row[:-1]
+        if i != 0:
+            inputs = [neuron['output'] for neuron in network[i - 1]]
+        for neuron in network[i]:
+            for j in range(len(inputs)):
+                neuron['weights'][j] += l_rate * neuron['delta'] * inputs[j]
+                neuron['weights'][-1] += l_rate * neuron['delta']
+
+# Treino efetuado a partir de um número fixo de epochs
+def train_network(network, train, l_rate, n_epoch, n_outputs):
+    for epoch in range(n_epoch):
+        sum_error = 0
+        for row in train:
+            outputs = forward_propagate(network, row)
+            expected = [0 for i in range(n_outputs)]
+            expected[row[-1]] = 1
+            sum_error += sum([(expected[i] - outputs[i]) ** 2 for i in range(len(expected))])
+            backward_propagate_error(network, expected)
+            update_weights(network, row, l_rate)
+        print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+
+# Inicialização da rede
+def initialize_network(n_inputs, n_hidden, n_outputs):
+    network = list()
+    hidden_layer = [{'weights': [random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+    network.append(hidden_layer)
+    output_layer = [{'weights': [random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+    network.append(output_layer)
+    return network
+
+
+# predição com uma rede
+def predict(network, row):
+    outputs = forward_propagate(network, row)
+    return outputs.index(max(outputs))
+
+
+# Algoritimo de retropropagação com gradiente estoccastico descendente
+def back_propagation(train, test, l_rate, n_epoch, n_hidden):
+    n_inputs = len(train[0]) - 1
+    n_outputs = len(set([row[-1] for row in train]))
+    network = initialize_network(n_inputs, n_hidden, n_outputs)
+    train_network(network, train, l_rate, n_epoch, n_outputs)
+    predictions = list()
+    for row in test:
+        prediction = predict(network, row)
+        predictions.append(prediction)
+    return (predictions)
+
+# Testa o algoritimo na base de dados de rodas
+seed(1)
+
+# Carrega os dados da tabela
+filename = 'seeds-dataset.csv'
+dataset = load_csv(filename)
+for i in range(len(dataset[0])-1):
+    str_column_to_float(dataset, i)
+
+# Converte classe em integrador
+str_column_to_int(dataset,len(dataset[0])-1)
+
+# Normaliza as variaveis de entrada
+minmax = dataset_minmax(dataset)
+normalize_dataset = (dataset, minmax)
+
+# avaliação final do algoritimo
+n_folds = 5
+l_rate = 0.3
+n_epoch = 500
+n_hidden = 5
+scores = evaluate_algorithm(dataset, back_propagation, n_folds, l_rate, n_epoch, n_hidden)
+print('Scores: %s' % scores)
+print('Mean Accuracy: %3f%%' % (sum(scores)/float(len(scores))))
